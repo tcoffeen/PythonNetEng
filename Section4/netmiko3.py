203890bbe6ba49f9613b0d12eddd8fc32d81b75e
tcoffeen <tom.coffeen@gmail.com>
2020-10-11 18:38:53 -0700

    modified netmiko scripts

--- Section4/netmiko3.py
+++ Section4/netmiko3.py
@@ -4,21 +4,21 @@ iosv_l2_s4 = {
     'device_type': 'cisco_ios',
     'ip': '172.16.128.84',
     'username': 'tom',
-    'password': 'cisco',
+    'password': 'cisco'
 }
 
 iosv_l2_s5 = {
     'device_type': 'cisco_ios',
     'ip': '172.16.128.85',
     'username': 'tom',
-    'password': 'cisco',
+    'password': 'cisco'
 }
 
 iosv_l2_s6 = {
     'device_type': 'cisco_ios',
     'ip': '172.16.128.86',
     'username': 'tom',
-    'password': 'cisco',
+    'password': 'cisco'
 }
 
 with open('iosv_l2_cisco_design') as f:
--- Section4/netmiko4.py
+++ Section4/netmiko4.py
@@ -2,37 +2,37 @@ from netmiko import ConnectHandler
 
 iosv_l2_s2 = {
     'device_type': 'cisco_ios',
-    'ip': '192.168.122.82',
-    'username': 'david',
-    'password': 'cisco',
+    'ip': '172.16.128.82',
+    'username': 'tom',
+    'password': 'cisco'
 }
 
 iosv_l2_s3 = {
     'device_type': 'cisco_ios',
-    'ip': '192.168.122.83',
-    'username': 'david',
-    'password': 'cisco',
+    'ip': '172.16.128.83',
+    'username': 'tom',
+    'password': 'cisco'
 }
 
 iosv_l2_s4 = {
     'device_type': 'cisco_ios',
-    'ip': '192.168.122.84',
-    'username': 'david',
-    'password': 'cisco',
+    'ip': '172.16.128.84',
+    'username': 'tom',
+    'password': 'cisco'
 }
 
 iosv_l2_s5 = {
     'device_type': 'cisco_ios',
-    'ip': '192.168.122.85',
-    'username': 'david',
-    'password': 'cisco',
+    'ip': '172.16.128.85',
+    'username': 'tom',
+    'password': 'cisco'
 }
 
 iosv_l2_s6 = {
     'device_type': 'cisco_ios',
-    'ip': '192.168.122.86',
-    'username': 'david',
-    'password': 'cisco',
+    'ip': '172.16.128.86',
+    'username': 'tom',
+    'password': 'cisco'
 }
 
 with open('iosv_l2_cisco_design') as f:
